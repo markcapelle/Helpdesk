7eb646f68cc3a6213b541516213ecf8b2479f42d
Mark Van Capelle <markcapelle@outlook.ie>
2026-07-28 22:13:24 +0100

    FEAT: extend auth_user with UserProfile table
    extending the existing django user database with UserProfile table that will contain some additional user data. Linked it as well to auth_groups which will act as a roles system within the app.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile"
+    )
+
+    avatar = models.TextField(blank=True, null=True)
+    phone = models.TextField(blank=True, null=True)
+
+    # Role_id > auth_group.id
+    role = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="profiles"
+    )
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
