0140009c02b4f2f62af3c1f5bbbadb5e24e0d61a
Mark Van Capelle <markcapelle@outlook.ie>
2026-08-04 21:34:34 +0100

    FEAT: routing for homepage
    created base routing to the homepage that the app will land on.

--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request, "homepage.html")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", homepage, name="homepage"),
+    path("admin/", admin.site.urls),
 ]
