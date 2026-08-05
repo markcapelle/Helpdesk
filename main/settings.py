e8287232a43ac983ded13186c38f98ba11609b04
Mark Van Capelle <markcapelle@outlook.ie>
2026-08-05 23:00:41 +0100

    BUILD: install whitenoise to help issues with staticfiles between django and render

--- main/settings.py
+++ main/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,3 +141,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "main" / "static",
 ]
+
+# WhiteNoise static file compression + manifest support
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
