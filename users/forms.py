e238f71c4c9dac9911e31c491d947c8448020a1d
Mark Van Capelle <markcapelle@outlook.ie>
2026-08-07 22:47:15 +0100

    FIX: removed old registration functions
    removed some functions and code for the old public registrations now that user registration is handled by admin.

--- users/forms.py
+++ users/forms.py
@@ -2,30 +2,6 @@ from django import forms
 from django.contrib.auth.models import User, Group
 from .models import UserProfile
 
-class UserRegistrationForm(forms.ModelForm):
-    password1 = forms.CharField(
-        label="Password",
-        widget=forms.PasswordInput
-    )
-    password2 = forms.CharField(
-        label="Confirm Password",
-        widget=forms.PasswordInput
-    )
-
-    class Meta:
-        model = User
-        fields = ["username", "first_name", "last_name", "email"]
-
-    def clean(self):
-        cleaned = super().clean()
-        p1 = cleaned.get("password1")
-        p2 = cleaned.get("password2")
-
-        if p1 != p2:
-            raise forms.ValidationError("Passwords do not match")
-
-        return cleaned
-
 
 class UserProfileForm(forms.ModelForm):
     COUNTRY_CODES = [
--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .forms import UserRegistrationForm, UserProfileForm, UserSelfServiceForm
+from .forms import UserProfileForm, UserSelfServiceForm
 
 
 # Login request
@@ -20,36 +20,6 @@ def login_page(request):
             messages.error(request, "Invalid username or password")
 
     return render(request, "login.html")
-
-
-# Register page
-def register_page(request):
-    if request.method == "POST":
-        user_form = UserRegistrationForm(request.POST)
-        profile_form = UserProfileForm(request.POST)
-
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save(commit=False)
-            user.set_password(user_form.cleaned_data["password1"])
-            user.save()
-
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            profile.save()
-
-            messages.success(request, "Account created successfully.")
-            return redirect("login")
-
-    else:
-        user_form = UserRegistrationForm()
-        profile_form = UserProfileForm()
-
-    return render(request, "users/user_form.html", {
-        "title": "Register New User",
-        "submit_label": "Register",
-        "user_form": user_form,
-        "profile_form": profile_form,
-    })
 
 
 # Login required to view dashboard
